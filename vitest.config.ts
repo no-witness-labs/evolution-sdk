7a4acf483d3272efc3d6007e1142fd9fb3cc1951
hade <liveithade@gmail.com>
2025-07-30 14:54:52 +0700

    fix: update test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vitest/config"
-
 export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    testTimeout: 60000,
     exclude: ["**/node_modules/**", "**/dist/**", "**/temp/**", "**/.{idea,git,cache,output,temp}/**"],
     coverage: {
       provider: "v8",

7d0c613598a01959327ce45b16ccae26e6583d71
solidsnakedev <jona.ca.eng@gmail.com>
2025-08-12 19:09:24 -0600

    test: improve vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vitest/config"
+
 export default defineConfig({
   test: {
     globals: true,
     environment: "node",
     testTimeout: 60000,
+    hookTimeout: 30000,
+    teardownTimeout: 30000,
+    pool: "forks",
+    poolOptions: {
+      forks: {
+        singleFork: true
+      }
+    },
+    retry: 2,
+    bail: 1,
     exclude: ["**/node_modules/**", "**/dist/**", "**/temp/**", "**/.{idea,git,cache,output,temp}/**"],
     coverage: {
       provider: "v8",

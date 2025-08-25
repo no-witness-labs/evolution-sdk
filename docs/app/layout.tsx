f6191f12773061107af14a7f0de102811faa84d3
solidsnakedev <jona.ca.eng@gmail.com>
2025-08-25 14:39:20 -0600

    feat: add search on the client side

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/app/global.css"
-import { RootProvider } from "fumadocs-ui/provider"
+// import { RootProvider } from "fumadocs-ui/provider"
+import { Provider } from "./provider"
+
 import { Inter } from "next/font/google"
 
 const inter = Inter({
@@ -10,7 +12,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   )
